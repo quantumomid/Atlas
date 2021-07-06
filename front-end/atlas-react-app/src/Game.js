ba8c99a3afaa11b209a07fd1b26d32769d3f86c1
David Ingram <davidingram9@gmail.com>
2021-07-06 15:56:48 +0100

    start game button calls random letter

--- front-end/atlas-react-app/src/Game.js
+++ front-end/atlas-react-app/src/Game.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
-    render() {
-      return (
-          <h1>This is the game</h1>
-      )
-    }
+  state = {
+    letter: ''
+  }
+
+  async startGame() {
+    const response = await fetch(`http://localhost:8080/letter`)
+    const initialLetter = await response.json()
+    console.log(initialLetter.letter)
+    this.setState({letter: initialLetter.letter})
+  }
+
+
+  render() {
+    const { letter } = this.state
+    return (
+      <main>
+        <button onClick={() => this.startGame()}>Start game</button>
+        {letter}
+      </main>
+    )
+  }
 }
 
 export default Game
