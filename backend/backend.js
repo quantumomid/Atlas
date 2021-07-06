4ecbb77a38ce914796f6099a2b891f2bdb393fc0
David Ingram <davidingram9@gmail.com>
2021-07-06 14:19:22 +0100

    backend framework finished

---
+++ backend/backend.js
@@ -0,0 +1,37 @@
+import { Application } from 'https://deno.land/x/abc@v1.3.1/mod.ts'
+import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts"
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
+import { cors } from 'https://deno.land/x/abc@v1.3.1/middleware/cors.ts'
+// import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts"
+import { DB } from 'https://deno.land/x/sqlite/mod.ts'
+import { config } from 'https://deno.land/x/dotenv/mod.ts'
+// import loginHandler from './loginHandler.js'
+
+const DENO_ENV = Deno.env.get('DENO_ENV') ?? 'development'
+
+config({ path: `./.env.${DENO_ENV}`, export: true })
+// const client = new Client(Deno.env.get('PG_URL'))
+// await client.connect()
+
+// turn on foreign key constraints
+db.query('PRAGMA foreign_keys = ON;')
+
+const db = new DB('./atlas.db')
+
+const app = new Application()
+const PORT = Number(Deno.env.get('PORT'))
+
+const headersWhitelist = [
+    "Authorization",
+    "Content-Type",
+    "Accept",
+    "Origin",
+    "User-Agent",
+]
+app.use(cors({ allowHeaders: headersWhitelist, allowCredentials: true, allowOrigins: Deno.env.get('ALLOWED_ORIGINS')}))
+
+app
+    //.post('/login', loginHandler)
+    .start({ port: PORT })
+
+console.log(`Server running on http://localhost:${PORT}`)
