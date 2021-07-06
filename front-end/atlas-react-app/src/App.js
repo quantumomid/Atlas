46392f37a0d162a77e4e2e39ed39386629235ba4
guy hotchin <guyhotchin@guys-MBP.home>
2021-07-06 12:32:17 +0100

    Draft React components structure

--- front-end/atlas-react-app/src/App.js
+++ front-end/atlas-react-app/src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import Homepage from './Hompage';
+import Game from './Game';
+import Registration from './Registration';
+import Login from './Login';
+import Logout from './Logout';
+
 
 class App extends Component {
     render() {
       return (
+        <div>
           <h1>Hello</h1>
+          <Homepage />
+          <Game />
+          <Registration />
+          <Login/>
+          <Logout/>
+        </div>  
       )
     }
 }
---
+++ front-end/atlas-react-app/src/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    render() {
+      return (
+          <h1>This is the game</h1>
+      )
+    }
+}
+
+export default Game
