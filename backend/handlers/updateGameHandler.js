963ee96494b15a77ff2bc406ea71f23cbdd4e1a7
David Ingram <davidingram9@gmail.com>
2021-07-08 16:09:10 +0100

    enforced unique countries on player

--- backend/handlers/updateGameHandler.js
+++ backend/handlers/updateGameHandler.js
@@ -10,6 +10,17 @@ config({ path: `./.env.${DENO_ENV}`, export: true })
 const client = new Client(Deno.env.get("PG_URL"))
 await client.connect()
 
+//helper function to input country played into countries_played
+async function insertToTable(countryArray, userInput, user) {
+    countryArray.push(userInput)
+
+    // re-stringify and update current_game table
+    await client.queryObject(`UPDATE current_games
+                              SET played_countries = $1
+                              WHERE username = $2;`, JSON.stringify(countryArray), user)
+
+}
+
 const updateGameHandler = async (server) => {
     // takes user country input
     // updates entry in current_games table
@@ -29,49 +40,56 @@ const updateGameHandler = async (server) => {
     // basic validations mirroring front end validations
     if (typeof userInput !== 'string' || userInput.length > 60 || userInput.length === 0) throw new Error('Bad userInput.')
 
+    // get current score of this game
+    const [[score]]  = (await client.queryArray(`SELECT score FROM current_games WHERE username = $1;`, user)).rows
+
     // find already played countries in this game
     let [[countryArray]] = (await client.queryArray(`SELECT played_countries FROM current_games WHERE username = $1;`, user)).rows
+    console.log('countryArray: ', countryArray)
 
     if (countryArray) countryArray = JSON.parse(countryArray) // parse the JSON stringified array
     if (!countryArray) countryArray = [] // if null (first turn), initialise as empty array
 
-    // add most recent input to array
-    countryArray.push(userInput)
+    if (countryArray.includes(userInput)) {
+        // if country has already been used this game, end the game
 
-    // re-stringify and update current_game table
-    await client.queryObject(`UPDATE current_games
-                              SET played_countries = $1
-                              WHERE username = $2;`, JSON.stringify(countryArray), user)
-
-    // check correctness of suggested country answer (disregarding case)
-    const [matches] = (await client.queryArray(`SELECT country_name 
-                                                FROM countries 
-                                                WHERE LOWER(country_name) = $1
-                                                AND SUBSTRING(country_name, 1, 1) = $2;`, userInput.toLowerCase(), letter)).rows
-
-    const [[score]]  = (await client.queryArray(`SELECT score FROM current_games WHERE username = $1;`, user)).rows
-
-    if (!matches) {
-        // if answer is incorrect, add to finished_games, delete from current_games, and return some response ***ADD SCORE***
-
+        await insertToTable(countryArray, userInput, user)
+        console.log('this country has been used, ending game')
         const correct = false
         await server.json({correct, score})
 
     } else {
-        // return a response with the letter for the next AI turn
-        // update score in current_games
-        // 1 is placeholder for whatever we decide a correct answer is worth!
-        await client.queryObject(`UPDATE current_games
-                                  SET score = $1
-                                  WHERE username = $2;`, score + 1, user)
-        // test:                                  
-        // let [[update]]  = (await client.queryArray(`SELECT score FROM current_games WHERE username = $1;`, user)).rows
-        // console.log('correct! updated score: ', scoreYes)
 
-        const correct = true
-        const lastLetter = userInput.slice(-1)
-        // console.log('lastLetter: ', lastLetter)
-        await server.json({correct, lastLetter, score: score + 1})
+        await insertToTable(countryArray, userInput, user)
+
+        // check correctness of suggested country answer (disregarding case)
+        const [matches] = (await client.queryArray(`SELECT country_name 
+                                                    FROM countries 
+                                                    WHERE LOWER(country_name) = $1
+                                                    AND SUBSTRING(country_name, 1, 1) = $2;`, userInput.toLowerCase(), letter)).rows
+
+        if (!matches) {
+            // if answer is incorrect, add to finished_games, delete from current_games, and return some response ***ADD SCORE***
+
+            const correct = false
+            await server.json({correct, score})
+
+        } else {
+            // return a response with the letter for the next AI turn
+            // update score in current_games
+            // 1 is placeholder for whatever we decide a correct answer is worth!
+            await client.queryObject(`UPDATE current_games
+                                    SET score = $1
+                                    WHERE username = $2;`, score + 1, user)
+            // test:                                  
+            // let [[update]]  = (await client.queryArray(`SELECT score FROM current_games WHERE username = $1;`, user)).rows
+            // console.log('correct! updated score: ', scoreYes)
+
+            const correct = true
+            const lastLetter = userInput.slice(-1)
+            // console.log('lastLetter: ', lastLetter)
+            await server.json({correct, lastLetter, score: score + 1})
+        }
     }
 }
 
