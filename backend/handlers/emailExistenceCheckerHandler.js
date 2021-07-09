bc715cc20c82e88130b0c8eb739edff3abe5d533
mbaugh99 <michaelbaugh99@gmail.com>
2021-07-09 17:44:55 +0100

    fix name of function in email existence handler

--- backend/handlers/emailExistenceCheckerHandler.js
+++ backend/handlers/emailExistenceCheckerHandler.js
@@ -7,7 +7,7 @@ config({ path: `./.env.${DENO_ENV}`, export: true })
 const client = new Client(Deno.env.get("PG_URL"))
 await client.connect()
 
-export default async function usernameExistanceCheckerHandler(server) {
+export default async function emailExistanceCheckerHandler(server) {
     const { email } = await server.body
     const [emailCheck] = (await client.queryArray(`SELECT 1 FROM users WHERE email = $1;`, email)).rows
     await server.json({uniqueEmail: !emailCheck})
