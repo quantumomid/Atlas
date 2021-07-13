729ffd6d39d0100d2374bdd0e9c1e656149dd561
mbaugh99 <michaelbaugh99@gmail.com>
2021-07-13 11:44:59 +0100

    renaming files

--- front-end/atlas-react-app/src/GameEndScreen.js
+++ front-end/atlas-react-app/src/GameEndScreen.js
@@ -1,12 +1,14 @@
 import { Component } from "react";
 import PersonalScoreBoard from "./PersonalScoreboard";
 import Registration from "./Registration";
+import './GameEndScreen.css'
 
 class GameEndScreen extends Component {
 
     state = {
         score: 0,
-        playedCountryArray: []
+        playedCountryArray: [],
+        finalCountry: ''
     }
 
     async componentDidMount() {
@@ -18,7 +20,35 @@ class GameEndScreen extends Component {
             }
         )
         const { score, playedCountryArray } = await response.json()
-        this.setState({score, playedCountryArray})
+        let finalCountry
+        if (this.props.time !== 0) finalCountry = playedCountryArray.pop()
+        this.setState({score, playedCountryArray, finalCountry})
+    }
+
+    renderReasonForLoss() {
+        const { playedCountryArray, finalCountry} = this.state
+        if (this.props.time === 0) {
+            return <h2>You ran out of time!</h2>
+        } else {
+            return (
+            <div className="final-input-container">
+                <h2>Your final played country was {finalCountry}</h2>
+                {playedCountryArray.includes(finalCountry) ? 
+                <h2>This country has already been played!</h2> :
+                <h2>That is not a valid country!</h2>}
+            </div> 
+            )
+        }
+        
+    }
+
+    renderCountriesList(countryList) {
+        const countryListElems = countryList.map((country, i) => <li key={i}>{country}</li>)
+        return (
+            <ul className='country-list'>
+                {countryListElems}
+            </ul>
+        )
     }
 
     render() {
@@ -26,13 +56,13 @@ class GameEndScreen extends Component {
         console.log('in end screen allMatches: ', allMatches)
         const { score, playedCountryArray } = this.state
         return (
-            <div className='endgame-screen'>
+            <div className='endgame-page'>
                 <div className='register-or-scoreboard'>
                     {isLoggedIn ? 
                     <PersonalScoreBoard 
                     score={score} /> 
                     : (
-                        <div>
+                        <div className='end-game-register'>
                             <h2>Register to save your score</h2>
                             <Registration
                             saveScore={true}
@@ -40,21 +70,24 @@ class GameEndScreen extends Component {
                         </div>
                     )}
                 </div>
-                <div>
-                    <h2>Your final score</h2>
-                    <p>{score}</p>
+                <div className='score-and-replay'>
+                    {this.renderReasonForLoss()}
+                    <h2>Final Score</h2>
+                    <p className='final-score'>{score}</p>
+                    <button onClick={() => this.props.handleGameReset()}> Play again</button>
                 </div>
-                {playedCountryArray.length !== 0 && 
-                <div>
-                    <h2>Your played countries</h2>
-                    <p>{playedCountryArray}</p>
-                </div>}
-                { allMatches.length !== 0 && 
-                <div>
-                    <h2>You could have played these!</h2>
-                    <p>{allMatches}</p>
-                </div>}
-                <button onClick={() => this.props.handleGameReset()}> Play again</button>
+                <div className='countries-played-and-could-container'>
+                    {playedCountryArray.length !== 0 && 
+                    <div>
+                        <h2>Your played countries</h2>
+                        {this.renderCountriesList(playedCountryArray)}
+                    </div>}
+                    { allMatches.length !== 0 && 
+                    <div>
+                        <h2>You could have played these!</h2>
+                        {this.renderCountriesList(allMatches)}
+                    </div>}
+                </div>
             </div>
         )
     }
