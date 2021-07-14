fbbfa3df6175fee4b12926f5962a1c426e4241a1
David Ingram <davidingram9@gmail.com>
2021-07-14 15:06:30 +0100

    added start game instructions, and readded logo files for chrome?

--- front-end/atlas-react-app/src/Game.js
+++ front-end/atlas-react-app/src/Game.js
@@ -268,6 +268,26 @@ class Game extends Component {
     
     return (
       <main className = 'game-page'>
+        {needStart && <div className="start-instructions">
+          <h3>Welcome to Atlas game!</h3>
+          <p>
+            On clicking start game, the AI will provide you with a random letter. <br />
+            For ten points, you will need to enter the name of a country beginning with that letter. <br />
+            If you are correct, you can then enter the name of that country's capital city for a bonus five points, or skip if you aren't sure.<br />
+            The AI will then pick a country that starts with the last letter of your chosen country.<br />
+            You then guess again, for the last letter of the AI's pick.
+          </p>
+          <p>
+            Any incorrect answers, or picking a country that has already been played by you or the AI, will end the game!<br />
+            If there are no remaining countries starting with a letter, we move on to the next letter in the alphabet instead, repeating until all 201 countries have been played.<br />
+          </p>
+          <p>
+            Don't let the timer run out or it's game over!
+          </p>
+          <h3>
+            Have fun!
+          </h3  >
+        </div>}
         <div className="start-button-container">
           {needStart && <button onClick={() => this.handleStartGame()}>Start Game</button>}
         </div>
@@ -288,7 +308,8 @@ class Game extends Component {
             {letter && !showCapitalCityQuestion ? <div className="main-question">Name a country beginning with:</div> : <div>For a bonus point, name the capital city of {formatUserGameInput(userInput)}</div>}
             {showCapitalCityQuestion ? 
             <div style={{ 
-              backgroundImage: `url(${this.state.flag})`, color:'white' }} className="letter"
+              backgroundImage: `url(${this.state.flag})`, color:'white' }} 
+              className="letter"
             >
               {letter}
             </div>
